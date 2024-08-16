09e9a5989a363456fb10ca4bc6ed3e94cd8446ba
br-photope <binaryraptor.project2@gmail.com>
2024-08-16 14:00:09 +0530

    Add files via upload

---
+++ src/components/AllCustomers.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function AllCustomers() {
+  return (
+    <div>
+      all Customers
+    </div>
+  )
+}
---
+++ src/components/Dashboard.jsx
@@ -0,0 +1,42 @@
+import React, { useContext, useEffect } from 'react'
+import Boxes from '../components/Boxes.jsx';
+import {useState} from 'react'; 
+import "./Dashboard.css";
+import { CustomersDataContext } from '../store/CustomersData.jsx';
+
+export default function Dashboard() {
+
+    const importedCustomersDataContext = useContext(CustomersDataContext);
+    
+  
+
+
+  return (
+    <>
+      <div id='iddb2'>
+
+        <div class='boxes' id='iddb1'>
+          <p>Total Business: {importedCustomersDataContext.totalCollection}</p>
+        </div>
+    
+    
+        <div class='boxes'>
+            <Boxes heading="New Customers" value={importedCustomersDataContext.countArr[3]}/>
+            <Boxes heading="New Orders" value={importedCustomersDataContext.countArr[4]}/>
+            <Boxes heading="Shoot Done" value={importedCustomersDataContext.countArr[9]}/>
+            <Boxes heading="Selection Done" value={importedCustomersDataContext.countArr[8]}/>
+            <Boxes heading="Editing Done" value={importedCustomersDataContext.countArr[2]}/>
+            <Boxes heading="Out for print" value={importedCustomersDataContext.countArr[5]}/>
+            <Boxes heading="Ready for pickup" value={importedCustomersDataContext.countArr[7]}/>
+            <Boxes heading="Delivered" value={importedCustomersDataContext.countArr[1]}/>
+            <Boxes heading="Delay Orders" value={importedCustomersDataContext.countArr[0]}/>
+            <Boxes heading="Pending Payments" value={importedCustomersDataContext.countArr[6]}/>
+            <Boxes heading="All Customers" value={importedCustomersDataContext.customersData.length}/>
+            <Boxes heading="Settings" value="NA"/>
+        </div>
+    
+
+      </div>
+    </>
+  )
+}
